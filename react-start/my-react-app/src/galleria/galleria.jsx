77fddcf5a2b240f9fc2da1ae0cdda3bed636e205
cskadam01 <167115402+cskadam01@users.noreply.github.com>
2024-10-27 18:33:52 +0100

    backend és regisztráció

--- react-start/my-react-app/src/galleria/galleria.jsx
+++ react-start/my-react-app/src/galleria/galleria.jsx
@@ -1,6 +1,41 @@
 function Galleria(){
     return(
-        <h1>Galleria mukodik</h1>
+
+        <div>
+        <h1>Eddigi munkáim</h1>
+            <div className="gall-content">
+                <div className="row">
+                    <div className="col-sm-12 col-md-6 col-lg-3">
+                        <div className="gall-card">
+
+                        </div>
+                    </div>
+                    <div className="col-sm-12 col-md-6 col-lg-3">
+                        <div className="gall-card">
+
+                        </div>
+                    </div>
+                    <div className="col-sm-12 col-md-6 col-lg-3">
+                        <div className="gall-card">
+                            
+                        </div>
+                    </div>
+                    <div className="col-sm-12 col-md-6 col-lg-3">
+                        <div className="gall-card">
+                            <img src="" alt="" />
+                            <h5>Francia</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut saepe aspernatur dignissimos quaerat odio? Nisi odio pariatur est minima hic magni rerum tempora dolore, reiciendis repudiandae maxime alias iusto illum.</p>
+                        </div>
+                    </div>
+                    <div className="col-sm-12 col-md-6 col-lg-3">
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
       
     );
 
